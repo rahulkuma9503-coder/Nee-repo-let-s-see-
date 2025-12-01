5e9dd05cfeac889f6166295083e4b2e8cbb16531
rahulkuma9503-coder <rahulkuma9503@gmail.com>
2025-12-01 11:33:35 +0530

    Update drm_handler_itsgolu.py

--- modules/drm_handler_itsgolu.py
+++ modules/drm_handler_itsgolu.py
@@ -293,7 +293,7 @@ async def drm_handler(bot: Client, m: Message):
          
             elif "https://cpvod.testbook.com/" in url or "classplusapp.com/drm/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = f"https://head-micheline-botupdatevip-f1804c58.koyeb.app/get_keys?url={url}@botupdatevip4u&user_id={user_id}"
+                url = f"https://itsgolu-v1player.vercel.app/?url={url}}"
                 result = helper.get_mps_and_keys2(url)
                 if result is None:
                     await m.reply_text(f"❌ Token failed. Trying next one...")
